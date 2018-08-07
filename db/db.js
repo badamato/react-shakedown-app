941da568d49be505c75b46c4e24488558e09372c
Beth Damato <bethadamato@gmail.com>
2018-08-07 16:24:39 -0400

    added get all categories in db.js

--- db/db.js
+++ db/db.js
@@ -5,10 +5,21 @@ const pgp = require('pg-promise')();
 //ENVIRONMENTAL VARIABLES
 const cn = {
   host: 'localhost',
-  port: 3500,
+  port: 5432,
   database: 'shakedown',
   user: 'postgres',
   password: ''
 };
+
 const db = pgp(cn);
 
+//SHOW++++++++++++++++++++++++++++++++
+
+
+function showAllCategories() {
+  return db.any(`SELECT * FROM Gear_Category`)
+}
+
+// showAllCategories()
+//   .then((data) => {console.log(data);})
+//   .catch((error) => {console.log(error);});
--- db/server.js
+++ db/server.js
@@ -2,6 +2,22 @@
 const express = require('express');
 const app = express();
 
+//IMPORT body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//IMPORT db.js
+const shakedown = require('./db');
+
+
+
+
+
+
+
+
+
+
 
 app.get('/api', (req, res) => {
     res.send('SERVER CONNECTED!')
