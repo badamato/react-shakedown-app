06687ca81872abca41a494a5483acd4ef034d653
Beth Damato <bethadamato@gmail.com>
2018-08-07 18:24:50 -0400

    chanced the gear_type db seed

--- db/db.js
+++ db/db.js
@@ -23,3 +23,21 @@ function showAllCategories() {
 // showAllCategories()
 //   .then((data) => {console.log(data);})
 //   .catch((error) => {console.log(error);});
+
+
+function showOneCategory(cat_id) {
+  return db.oneOrNone(`SELECT * FROM Gear_Category where cat_id= $1`, [cat_id])
+}
+
+// showOneCategory(2)
+//   .then((data) => {console.log(data);})
+//   .catch((error) => {console.log(error);});
+
+
+function showAllCatTypes(cat_id) {
+  return db.any(`SELECT * FROM Gear_Type WHERE cat_id=$1`, [cat_id])
+}
+
+showAllCatTypes(2)
+  .then((data) => {console.log(data);})
+  .catch((error) => {console.log(error);});
--- db/test-queries.sql
+++ db/test-queries.sql
@@ -1,11 +1,11 @@
 --CATEGORIES++++++++++++++++
 
 -- get all the categories
-SELECT * FROM Gear_Categories;
+SELECT * FROM Gear_Category;
 
 
 -- get one category by id
-SELECT * FROM Gear_Categories WHERE cat_id=2;
+SELECT * FROM Gear_Category WHERE cat_id=2;
 
 
 -- get all types (in a single category)
