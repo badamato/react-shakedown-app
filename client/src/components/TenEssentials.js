ae75fc8bffc6903886701bad7d7cf7ffde5ae1d2
Aylin DeBruyne <adebruyne88@gmail.com>
2018-08-07 22:41:21 -0400

    create the ten essentials list

--- client/src/components/TenEssentials.js
+++ client/src/components/TenEssentials.js
@@ -6,18 +6,68 @@ class TenEssentials extends React.Component {
     return (
       <div>
         <h1>The Ten Essentials</h1>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
-          <li>8</li>
-          <li>9</li>
-          <li>10</li>
-        </ul>
+        <p>
+          If you're heading into the wilderness, you're going to want to take
+          the 10 essentials with you. Even on a day hike, it's good practice.
+          You'll find the true value of carrying these items in a survival
+          situtation. They'll ensure you're prepared in an emergency and keep
+          you safe if you're forced to spend extra time out in the wild!
+        </p>
+        <p>
+          Depending on the type of trip you're taking, you can take a
+          combination of the items in each category.
+        </p>
+        <div>
+          <h4>Navigation</h4>
+          <ul>
+            <li>Maps</li>
+            <li>Compass</li>
+            <li>Handheld GPS</li>
+            <li>Personal Locator Beacon</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Illumination</h4>
+          <ul>
+            <li>Headlamp</li>
+            <li>Lantern</li>
+            <li>Flashlight</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Sun Protection</h4>
+          <ul>
+            <li>Sunglasses</li>
+            <li>Sunscreen</li>
+            <li>Sun hat</li>
+          </ul>
+        </div>
+        <div>
+          <h4>First Aid</h4>
+        </div>
+        <div>
+          <h4>Repair Kit and Tools</h4>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            </ul>
+        </div>
+        <div>
+          <h4>Fire</h4>
+        </div>
+        <div>
+          <h4>Insulation</h4>
+        </div>
+        <div>
+          <h4>Nutrition</h4>
+        </div>
+        <div>
+          <h4>Hydration</h4>
+        </div>
+        <div>
+          <h4>Emergency Shelter</h4>
+        </div>
       </div>
     );
   }
