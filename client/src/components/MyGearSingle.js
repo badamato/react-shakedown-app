f50efde9aae72f85ccc22ee107e1944e2eb1544a
Aylin DeBruyne <adebruyne88@gmail.com>
2018-08-13 12:40:36 -0400

    got image to fill on mygear route card

--- client/src/components/MyGearSingle.js
+++ client/src/components/MyGearSingle.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class MyGearSingle extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.defaultImg = "https://static.thenounproject.com/png/254126-200.png"
   }
   render() {
     return (
       <div className="container my-gear-card">
-        <img src="https://static.thenounproject.com/png/254126-200.png" className="img-thumbnail gear-img"/>
+        <img src={this.props.my_gear_img || this.defaultImg}  className="img-thumbnail gear-img"/>
         <h4><strong>Name</strong>: {this.props.my_gear_name}</h4>
         <h5><strong>Weight</strong>:  {this.props.my_gear_weight} oz.</h5>
       </div>
