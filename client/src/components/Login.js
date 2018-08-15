d0b74109269ed3bb47c732c9aaf8a979f2a5e9cd
Aylin DeBruyne <adebruyne88@gmail.com>
2018-08-15 13:05:09 -0400

    styling added to login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,76 +1,77 @@
-import React from 'react';
-import axios from 'axios';
-
+import React from "react";
+import axios from "axios";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    render() {
-        return (
-            <div className="signup-wrapper">
-                <form 
-                className="signup-form"
-                onSubmit={this._handleSubmit}>
-                <label for="">username:
-                <input 
-                    type="text" 
-                    name="email" 
-                    onChange={this._handleEmail}
-                    value={this.state.email}
-                />
-                </label>
-                <br />
-                <label for="">password:
-                <input 
-                    type="password" 
-                    name="password" 
-                    onChange={this._handlePassword}
-                    value={this.state.password}
-                />
-                </label>
-                <br />
-                <input type="submit" />
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="signup-wrapper">
+        <form className="signup-form gear-form" onSubmit={this._handleSubmit}>
+         <h1>Welcome to Shakedown!</h1>
+         <p>Sign In</p>
+         <label for="">
+            username:
+            <input
+              type="text"
+              name="email"
+              onChange={this._handleEmail}
+              value={this.state.email}
+            />
+          </label>
+          <br />
+          <label for="">
+            password:
+            <input
+              type="password"
+              name="password"
+              onChange={this._handlePassword}
+              value={this.state.password}
+            />
+          </label>
+          <br />
+          <input type="submit" className="hit-it" />
+         <p>New to Shakedown? Let's sign you up! </p> 
+        </form>
+        
+      </div>
+    );
+  }
 
-    _handleEmail = (event) => {
-        const value = event.target.value;
+  _handleEmail = event => {
+    const value = event.target.value;
 
-        this.setState({
-            email: value
-        }) 
-    }
+    this.setState({
+      email: value
+    });
+  };
 
-    _handlePassword = (event) => {
-        const value = event.target.value;
+  _handlePassword = event => {
+    const value = event.target.value;
 
-        this.setState({
-            password: value
-        }) 
-    }
+    this.setState({
+      password: value
+    });
+  };
 
-    _handleSubmit = (event) => {
-        event.preventDefault();
+  _handleSubmit = event => {
+    event.preventDefault();
 
-        axios.post('/api/login/', this.state)
-            .then((response) => {
-                console.log(response.data);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
-
-
-
-    }
+    axios
+      .post("/api/login/", this.state)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+}
 
 export default Login;
